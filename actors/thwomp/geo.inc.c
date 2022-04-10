d5a160754c6eb67c91ce1ae0f2855eb5593ec0c9
swc64boi <101369269+swc64boi@users.noreply.github.com>
2022-04-10 08:51:17 -0500

    Add files via upload

--- actors/thwomp/geo.inc.c
+++ actors/thwomp/geo.inc.c
@@ -1,8 +1,18 @@
-// 0x0C000248
+/********************************************************************************
+		RCP_HmsDosun
+														[ Oct 23, 1995 ]
+*********************************************************************************/
+
+extern Gfx gfx_dosun[];
+
+
+/********************************************************************************/
+/*	Hierarchy map data of Dosun.												*/
+/********************************************************************************/
 const GeoLayout thwomp_geo[] = {
    GEO_SHADOW(SHADOW_SQUARE_SCALABLE, 0xB4, 300),
    GEO_OPEN_NODE(),
-      GEO_DISPLAY_LIST(LAYER_OPAQUE, thwomp_seg5_dl_0500B750),
+      GEO_DISPLAY_LIST(LAYER_OPAQUE, gfx_dosun),
    GEO_CLOSE_NODE(),
    GEO_END(),
 };
